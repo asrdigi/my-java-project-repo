d776022047b712a5861d3314b38f2b0649ff75f0
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2020-09-05 23:57:47 +0530

    first commit

---
+++ src/test/java/com/sapient/app/AppTest.java
@@ -0,0 +1,27 @@
+package com.sapient.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Test;
+public class AppTest {
+   
+    @Test
+    public void shouldAnswerWithTrue()    {
+        assertTrue( true );
+    }
+    
+    private String INPUT = "123456";
+
+    @Test
+    public void testLength() {
+        Assert.assertEquals(64, App.sha256hex(INPUT).length());
+    }
+
+    @Test
+    public void testHex() {
+        String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+        Assert.assertEquals(expected, App.sha256hex(INPUT));
+    }
+}
